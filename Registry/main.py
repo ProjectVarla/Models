35dc27e27c193cc6b6dc9f1f4a4b8e21e6c896f9
Mohammad Rimawi <mohrimawiz@gmail.com>
2023-10-06 18:54:49 +0300

    Pushing to Remote

--- Registry/main.py
+++ Registry/main.py
@@ -2,6 +2,8 @@ from typing import Any, List, Optional
 
 from pydantic import BaseModel
 
+from VarlaLib.Shell import VarlaCLI as Varla
+
 
 class Command(BaseModel):
     command: str
@@ -15,5 +17,15 @@ class Registry(BaseModel):
     command: Command
     subcommands: Optional[List[Command]] = []
 
-    def trigger(self) -> None:
-        self.command.action()
+    def trigger(self, *sub_command) -> None:
+
+        try:
+            if len(*sub_command):
+                self.command.action(*sub_command)
+            else:
+                self.command.action()
+
+        except Exception as e:
+            Varla.say(str(self.command.__dict__))
+            Varla.error(str(e))
+            pass
